62ea6c4f73be1616d314d26e526b6a9e0ce3eb8a
Karl Pietri <barnaclebob@gmail.com>
2016-02-01 12:51:16 -0600

    fix up the 2 vhosts thing

--- manifests/vhost/nginx.pp
+++ manifests/vhost/nginx.pp
@@ -21,47 +21,31 @@ class smforum::vhost::nginx(
     include '::php::extension::apc'
   }
 
-  $redirect_rule = { 'rewrite' => '^ https://$server_name$request_uri? permanent' }
-
-  $http_redirect = $ssl_only ? {
-    true    => $redirect_rule,
-    default => undef,
+  nginx::resource::vhost { $vhost_fqdn:
+    ensure      => present,
+    www_root    => $document_root,
+    index_files => [ 'index.php' ],
+    ssl         => true,
+    ssl_cert    => $ssl_cert,
+    ssl_key     => $ssl_key,
+    use_default_location => false,
+    rewrite_to_https => true,
   }
 
-#  nginx::resource::vhost { "${vhost_fqdn} http":
-#    ensure              => present,
-#    www_root            => $document_root,
-#    location_cfg_append => $http_redirect,
-#  }
-
-  if $ssl {
-    nginx::resource::vhost { "${vhost_fqdn} ssl":
-      ensure      => present,
-#      listen_port => 443,
-      www_root    => $document_root,
-      index_files => [ 'index.php' ],
-      ssl         => true,
-      ssl_cert    => $ssl_cert,
-      ssl_key     => $ssl_key,
-      use_default_location => false,
-      rewrite_to_https => true,
-    }
+  nginx::resource::location { "${vhost_fqdn}_root":
+    ensure              => present,
+    ssl                 => true,
+    ssl_only            => $ssl_only,
+    vhost               => $vhost_fqdn,
+    www_root            => $document_root,
+    location            => '~ \.php$',
+    index_files         => ['index.php', 'index.html', 'index.htm'],
+    fastcgi             => $php_fcgi,
+    fastcgi_script      => undef,
+    location_cfg_append => {
+      fastcgi_connect_timeout => '3m',
+      fastcgi_read_timeout    => '3m',
+      fastcgi_send_timeout    => '3m',
+    },
   }
-
-#  nginx::resource::location { "${vhost_fqdn}_root":
-#    ensure              => present,
-#    ssl                 => true,
-#    ssl_only            => $ssl_only,
-#    vhost               => "${vhost_fqdn} ssl",
-#    www_root            => $document_root,
-#    location            => '~ \.php$',
-#    index_files         => ['index.php', 'index.html', 'index.htm'],
-#    fastcgi             => $php_fcgi,
-#    fastcgi_script      => undef,
-#    location_cfg_append => {
-#      fastcgi_connect_timeout => '3m',
-#      fastcgi_read_timeout    => '3m',
-#      fastcgi_send_timeout    => '3m',
-#    },
-#  }
 }
